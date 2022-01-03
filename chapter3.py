f949e1da58001431d355c077a098fba37fbce777
Notis Siokas <76698442+NotisSiokas@users.noreply.github.com>
2022-01-03 19:24:55 +0000

    more features

--- chapter3.py
+++ chapter3.py
@@ -7,7 +7,12 @@ print(img.shape)
 imgResize = cv2.resize(img,(300,200))
 print(imgResize.shape)
 
+imgCropped = img[0:200, 200:500]
+print(imgCropped.shape)
+
+
 cv2.imshow("Image",img)
 cv2.imshow("Image Resize", imgResize)
+cv2.imshow("Image Cropped", imgCropped)
 
 cv2.waitKey(0)
