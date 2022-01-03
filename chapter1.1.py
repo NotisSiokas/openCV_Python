61cebd094a40706f5367ce225f4cabd8f77578e7
Notis Siokas <76698442+NotisSiokas@users.noreply.github.com>
2022-01-03 19:01:52 +0000

    more features

--- chapter1.1.py
+++
@@ -1,5 +0,0 @@
-import cv2
-
-img = cv2.imread("Resources/obi.png")
-
-imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
--- chapter1.py
+++ chapter1.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+
 
 # !!  SHOW AN IMAGE  !!
 # print("Package Imported")
@@ -18,14 +20,32 @@ import cv2
 #     if cv2.waitKey(1) & 0xFF ==ord('q'):
 #         break
 
+# !! SHOW WEBCAM !!
+# cap =cv2.VideoCapture(0)
+# cap.set(3,640)
+# cap.set(4,480)
+# cap.set(10,100)
+#
+# while True:
+#     success, img = cap.read()
+#     cv2.imshow("Video", img)
+#     if cv2.waitKey(1) & 0xFF ==ord('q'):
+#         break
 
-cap =cv2.VideoCapture(0)
-cap.set(3,640)
-cap.set(4,480)
-cap.set(10,100)
+img = cv2.imread("Resources/obi.png")
+kernel = np.ones((5,5),np.uint8)
 
-while True:
-    success, img = cap.read()
-    cv2.imshow("Video", img)
-    if cv2.waitKey(1) & 0xFF ==ord('q'):
-        break
+
+imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)
+imgCanny = cv2.Canny(img,150,200)
+imgDilation = cv2.dilate(imgCanny,kernel,iterations=1)
+imgEroded = cv2.erode(imgDilation, kernel, iterations=1)
+
+
+cv2.imshow("Gray Image",imgGray)
+cv2.imshow("Blur Image",imgBlur)
+cv2.imshow("Canny Image", imgCanny)
+cv2.imshow("Dilation Image", imgDilation)
+cv2.imshow("Eroded Image", imgEroded)
+cv2.waitKey(0)
